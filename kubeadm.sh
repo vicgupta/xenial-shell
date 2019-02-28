e93c486d08316387024fc58c5b3d0c8ada92225c
Vic Gupta <vicgupta@gmail.com>
2019-02-28 12:09:26 -0800

    Initial

---
+++ kubeadm.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+
+#install kubeadm, kubectl, kubelet
+
+sudo apt update
+sudo apt install -y docker.io 
+sudo apt install -y software-properties-common apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.kubernetes.io kubernetes-xenial main"
+sudo apt-get update
+sudo apt-get install -y kubeadm kubectl kubelet
+sudo swapoff -a
+sudo apt-get upgrade -y
