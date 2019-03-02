c494cc02f1887317fcede2117e278d2849eeb045
Vic Gupta <vicgupta@gmail.com>
2019-03-01 17:00:49 -0800

    Added systemctl for docker

--- kubeadm.sh
+++ kubeadm.sh
@@ -5,6 +5,7 @@
 
 sudo apt update
 sudo apt install -y docker.io 
+sudo systemctl enable docker.service
 sudo apt install -y software-properties-common apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.kubernetes.io kubernetes-xenial main"
